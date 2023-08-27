4ae5e36a52d5260d0796a039ed5261e9f5ba15fc
kayk <0001000287@senaimgaluno.com.br>
2023-08-27 15:20:38 -0300

    sequencia decrescente

--- sequencia.py
+++ sequencia.py
@@ -4,3 +4,16 @@ incremento = 25
 # Loop para gerar a sequência
 for i in range(0, 201, incremento):
     print(i, end=", ")
+
+# Inicializa o primeiro número da sequência
+numero = 200
+
+
+# Loop para gerar a sequência decrescente
+while numero > 25:
+    print(numero, end=", ")
+    numero //= 2
+
+
+# Imprime o último número da sequência (25)
+print(numero)
